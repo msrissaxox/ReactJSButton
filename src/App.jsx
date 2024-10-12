8b97974efa0683512624ec4c518bfb4f32064f59
msrissaxox <marissa.bianca.lamothe@gmail.com>
2024-10-12 00:56:20 -0400

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+'use client'
+
+import { useState } from 'react'
+import Btn from './Btn'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './App.css'; 
+const App = () => {
+  return (
+    <div>
+   <Btn />
+    </div>
+  )
+}
+
+export default App
+
+
+  
+
+
---
+++ src/Btn.jsx
@@ -0,0 +1,16 @@
+
+// import React, { useEffect } from "react";
+// import { loadConfigFromFile } from "vite";
+
+import React from "react";
+import ReactDOM from 'react-dom';
+
+export default function Btn() {
+
+
+    return (
+  <>
+     <button onclick="buttonChanger()" id="special_Btn">Press Me</button>
+    </>
+    )};
+
